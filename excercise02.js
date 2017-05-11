850b7cabc088e5b95815dc683e9568f7607fe20c
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-05-11 07:20:58 +0200

    Excercise02

---
+++ excercise02.js
@@ -0,0 +1,48 @@
+//Create a variable called carName, assign the value "Volvo" to it, and display it.
+
+//Create a variable called number, assign the value 50 to it, and display it.
+
+//The code below should display "Volvo" - Fix it.
+
+var carName = "Volvo";
+
+console.log(carname);
+
+//Display the sum of 5 + 10, using two variables x and y.
+
+//Create a third variable called z, assign x + y to it, and display it.
+
+//Find the remainder when 15 is divided by 9, using two variables x and y.
+
+//Use the += operator to add a value of 5 to the variable x.
+
+//call the following function
+
+function myFunction1() {
+    console.log("Hello World!");
+}
+
+//Figure out what is wrong with the function - fix it and run it as it should!
+
+func myFunction2 {
+    console.log("Hello World!");
+}
+
+//Use the function to display the product of 5 * 5.
+
+function myFunction3() {
+    // Add code here
+}
+console.log(myFunction3());
+
+//Use the function to display "Hello John".
+
+function myFunction4(name) {
+    return "Hello " + name;
+}
+
+//Define a function named "myFunction5", and make it display "Hello World!" in the console.
+
+//Define a function that takes 2 params num1 and num2
+//let the function return true if the num1 is greater than num2, false otherwise
+
