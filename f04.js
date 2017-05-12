88c4a5c1d3481dde1589703249928f93f596ed65
Simona Colapicchioni <s.colapicchioni@gmail.com>
2017-05-12 10:12:39 +0200

    Day03

---
+++ f04.js
@@ -0,0 +1,104 @@
+//objects
+
+//string literal
+// ""
+
+let s = "";
+
+
+// array literal
+// []
+
+let a = [];
+
+//object literal
+// {}
+
+let o = {}; //empty object
+
+//JavaScript Object Notation (JSON)
+
+let car1 = {
+    brand : "FIAT",
+    name : "500",
+    price : 10000,
+    on : false,
+    passengers : [
+        {
+            name : "Simona",
+            age : 43
+        },
+        {
+            name : "Jan",
+            age : 5
+        },
+        {
+            name : "Remko",
+            age : 37
+        }
+    ],
+    engine : {
+        name : "V8",
+        acceleration: 200
+    }
+};
+
+//car1.engine.acceleration
+
+console.log(car1.name);
+console.log(car1.brand);
+
+console.log(car1.engine.name);
+console.log(car1.passengers[0].name);
+
+
+console.log(car1["name"]);
+
+//let speed = 10;
+
+let car2 = {
+    brand : "FERRARI",
+    name : "Testarossa",
+    speed : 0,
+    maxSpeed : 200,
+    // accelerate : function (toHowMuch){
+    //     this.speed = toHowMuch;
+    // } 
+    accelerate (toHowMuch){
+        this.speed = toHowMuch;
+    }
+
+};
+
+console.log(car2.speed);
+car2.accelerate(50);
+console.log(car2.speed);
+
+car1.passengers.forEach(p=>console.log(p.name));
+
+let person = {
+    "name" : "Simona",
+    "address-1" : ""
+};
+
+person["address-1"];
+
+// car1.brand
+// car2.brand
+
+
+
+function changeStyle(backgroundColor, color, fontName, fontSize, text){
+    //do something with backgroundColor;
+
+}
+
+changeStyle(undefined, undefined, undefined, undefined, "HI");
+
+changeStyleObjectOriented({backgroundColor: "red", color : "yellow", fontName : "Arial", fontSize : 16, text: "HI"});
+
+changeStyleObjectOriented({text: "HI", color: "red"});
+
+function changeStyleObjectOriented(options){
+    //do something with options.backgroudColor
+}
